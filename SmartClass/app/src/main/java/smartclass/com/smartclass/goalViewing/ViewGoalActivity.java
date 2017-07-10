f9e821c8c8f096753659f5fd9b2a42acdf3f68a4
Kevin Truong <truong.kevin3@gmail.com>
2017-07-09 23:32:24 -0400

    Created the xml layout for viewing the details of a goal

---
+++ SmartClass/app/src/main/java/smartclass/com/smartclass/goalViewing/ViewGoalActivity.java
@@ -0,0 +1,18 @@
+package smartclass.com.smartclass.goalViewing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import smartclass.com.smartclass.R;
+
+/**
+ * This Activity displays the details of a goal
+ */
+public class ViewGoalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_goal);
+    }
+}
