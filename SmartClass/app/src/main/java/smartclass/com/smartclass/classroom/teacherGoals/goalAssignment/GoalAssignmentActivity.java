a0794980761c7bb0bea7145df565107b4a8baee1
Peter Pogorski <ppogorsk@uwaterloo.ca>
2017-07-16 18:04:46 -0400

    Add login checks

---
+++ SmartClass/app/src/main/java/smartclass/com/smartclass/classroom/teacherGoals/goalAssignment/GoalAssignmentActivity.java
@@ -0,0 +1,19 @@
+package smartclass.com.smartclass.classroom.teacherGoals.goalAssignment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by peterpogorski on 2017-07-16.
+ */
+
+public class GoalAssignmentActivity extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //setContentView();
+    }
+}
---
+++ SmartClass/app/src/main/java/smartclass/com/smartclass/classroom/teacherGoals/goalAssignment/GoalAssignmentPresenter.java
@@ -0,0 +1,11 @@
+package smartclass.com.smartclass.classroom.teacherGoals.goalAssignment;
+
+/**
+ * Created by peterpogorski on 2017-07-16.
+ */
+
+public class GoalAssignmentPresenter {
+
+
+
+}
