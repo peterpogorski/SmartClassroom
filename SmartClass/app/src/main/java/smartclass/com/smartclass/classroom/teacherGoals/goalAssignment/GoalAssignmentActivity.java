557029b8c0bfb680b91e3a7ea2828c47df33ab71
Peter Pogorski <ppogorsk@uwaterloo.ca>
2017-07-17 10:08:18 -0400

    Add activity to manifest

--- SmartClass/app/src/main/java/smartclass/com/smartclass/classroom/teacherGoals/goalAssignment/GoalAssignmentActivity.java
+++ SmartClass/app/src/main/java/smartclass/com/smartclass/classroom/teacherGoals/goalAssignment/GoalAssignmentActivity.java
@@ -4,16 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import smartclass.com.smartclass.R;
+
 /**
  * Created by peterpogorski on 2017-07-16.
  */
 
-public class GoalAssignmentActivity extends Activity{
+public class GoalAssignmentActivity extends Activity implements GoalAssignmentContract.View {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //setContentView();
+        setContentView(R.layout.activity_goal_assignment);
     }
+
 }
